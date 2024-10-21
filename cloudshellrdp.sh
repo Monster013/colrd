2f08b88201f22607e1c19026534d8900099c9f2d
Monster013 <1monsteruniverse@gmail.com>
2024-10-22 04:19:00 +0530

    Update cloudshellrdp.sh

--- cloudshellrdp.sh
+++ cloudshellrdp.sh
@@ -1,16 +1,6 @@
-rm -rf ngrok  ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-echo "Download ngrok"
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Ctrl + V Authtoken: " CRP 
-./ngrok authtoken $CRP 
-nohup ./ngrok tcp 3389 &>/dev/null &
-./ngrok tcp 3389 &>/dev/null &
 echo Downloading File From akuh.net
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F > /dev/null 2>&1
-sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main" > /dev/null 2>&1
 sudo apt update > /dev/null 2>&1
-sudo apt-get install firefox -y > /dev/null 2>&1
 echo "===================================="
 echo "Download RDP"
 echo "===================================="
